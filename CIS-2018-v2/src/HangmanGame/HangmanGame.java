747cf312fdf9449c536c74cd64bad966c53b4c7f
unknown <Zedaggett@gmail.com>
2018-10-05 02:20:27 -0400

    Progress 2

--- CIS-2018-v2/src/HangmanGame/HangmanGame.java
+++ CIS-2018-v2/src/HangmanGame/HangmanGame.java
@@ -16,9 +16,15 @@ public class HangmanGame {
 	public static void main (String[] args){}
 	public void createMask()
 	{
-		String word = "Maine";
+		{
+			System.out.print("---Welcome to Hangman---"); //Welcome message
+		}
+		{
+			System.out.println("");
+		}
+		String word = "Maine"; //Picked a word for my hangman game
 		char[] charArray=word.toCharArray();
-		for (int i = 0; i < word.length(); i++)
+		for (int i = 0; i < word.length(); i++) //Creating a mask for "Maine"
 		{
 			System.out.print ("*");
 		}
@@ -26,11 +32,11 @@ public class HangmanGame {
 			System.out.println(" ");
 		}
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //Creating a new Scanner to accept user input
 		{
 			System.out.print("Guess a letter :");
 		}
-		String guess = sc.next();
+		String guess = sc.next(); //Assigning a value to the user's input
 		
 		{
 			System.out.print(sc.nextLine());
