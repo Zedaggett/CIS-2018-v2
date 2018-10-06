b8d2fc58392ba5ab1360bc6c016c691965086b9c
unknown <Zedaggett@gmail.com>
2018-10-06 00:00:32 -0400

    Progress 3

--- CIS-2018-v2/src/HangmanGame/HangmanGame.java
+++ CIS-2018-v2/src/HangmanGame/HangmanGame.java
@@ -13,37 +13,68 @@ public class HangmanGame {
 	 * Hint limit = 1
 	 * 
 	 */
-	public static void main (String[] args){}
-	public void createMask()
-	{
+	Scanner sc = new Scanner(System.in);
+	public static void main (String[] args){
 		{
 			System.out.print("---Welcome to Hangman---"); //Welcome message
 		}
 		{
 			System.out.println("");
 		}
-		String word = "Maine"; //Picked a word for my hangman game
-		char[] charArray=word.toCharArray();
-		for (int i = 0; i < word.length(); i++) //Creating a mask for "Maine"
+
+	String word = "Maine";
+	String[] wordParts = word.split("");
+	
+	{
+		
+		String m = wordParts[0];
+		for (int i = 0; i < m.length() ; i++);
+		boolean guessM = false; // Figure out how to use boolean to trigger mask removal
+		do{
+			System.out.print("*");
+		} while (guessM = false);
+		
+		boolean guessA = false;
+		String a = wordParts[1];
+		for (int i = 0; i < a.length(); i++);
+		do {
+			System.out.print("*");
+		} while (guessA = false);
+		
+	String I = wordParts[2];
+		for (int i = 0; i < I.length(); i++);
 		{
-			System.out.print ("*");
+			System.out.print("*");
+		}
+	String n = wordParts[3];
+		for (int i = 0; i < n.length(); i++);
+		{
+			System.out.print("*");
+		}
+	String e = wordParts[4];
+		for (int i = 0; i <e.length(); i++);
+		{
+			System.out.print("*");
+		}
+		{
+			System.out.println(" ");
+		}
+		{
+			System.out.print("Guess a letter : ");
+		}
+		
+		
+	Scanner sc = new Scanner(System.in);
+		 guess = sc.nextLine(); // find correct variable here
+		{
+			System.out.print("The user has guessed : " + guess);
 		}
 		{
 			System.out.println(" ");
 		}
 		
-		Scanner sc = new Scanner(System.in); //Creating a new Scanner to accept user input
-		{
-			System.out.print("Guess a letter :");
-		}
-		String guess = sc.next(); //Assigning a value to the user's input
-		
-		{
-			System.out.print(sc.nextLine());
-		}
-		sc.close();
-		
-	}
+}
+}
 }
 
 		
--- CIS-2018-v2/src/HangmanGame/HangmanGameApplication.java
+++ CIS-2018-v2/src/HangmanGame/HangmanGameApplication.java
@@ -8,7 +8,35 @@ public class HangmanGameApplication {
 		 *If answer is true, apply to current game state
 		 *Repeat 
 		 */
-		HangmanGame newG = new HangmanGame(); //Creating a new instance of a game of Hangman
-		newG.createMask();
+		
 	}
 }
+String m = wordParts[0];
+for (int i = 0; i < m.length(); i++);
+{
+	System.out.print("*");
+}
+String a = wordParts[1];
+for (int i = 0; i < a.length(); i++);
+{
+	System.out.print("*");
+}
+String I = wordParts[2];
+for (int i = 0; i < I.length(); i++);
+{
+	System.out.print("*");
+}
+String n = wordParts[3];
+for (int i = 0; i < n.length(); i++);
+{
+	System.out.print("*");
+}
+String e = wordParts[4];
+for (int i = 0; i <e.length(); i++);
+{
+	System.out.print("*");
+}
+{
+	System.out.println(" ");
+}
+{
