597609d062b01ab0aa1640f1252697b06f4ba733
unknown <Zedaggett@gmail.com>
2018-10-08 10:01:44 -0400

    Regress 1

--- CIS-2018-v2/src/HangmanGame/HangmanGame.java
+++ CIS-2018-v2/src/HangmanGame/HangmanGame.java
@@ -1,82 +1,50 @@
 package HangmanGame;
 
 import java.util.Scanner;
-
 public class HangmanGame {
 
 	
-	/*
-	 * Rules that need to be followed:
-	 * Game initiate
-	 * Choose word
-	 * Guess limit = 5
-	 * Hint limit = 1
-	 * 
-	 */
-	Scanner sc = new Scanner(System.in);
-	public static void main (String[] args){
-		{
-			System.out.print("---Welcome to Hangman---"); //Welcome message
-		}
-		{
-			System.out.println("");
-		}
-
-	String word = "Maine";
-	String[] wordParts = word.split("");
+	public static void main (String[] args) {
+HangmanGame.hangGame = new HangmanGame();
+hangGame.isLetterGuessed();
 	
-	{
-		
-		String m = wordParts[0];
-		for (int i = 0; i < m.length() ; i++);
-		boolean guessM = false; // Figure out how to use boolean to trigger mask removal
-		do{
-			System.out.print("*");
-		} while (guessM = false);
-		
-		boolean guessA = false;
-		String a = wordParts[1];
-		for (int i = 0; i < a.length(); i++);
-		do {
-			System.out.print("*");
-		} while (guessA = false);
-		
-	String I = wordParts[2];
-		for (int i = 0; i < I.length(); i++);
+		/*Variables
+		 * String word = "Maine"
+		 * String input = sc.nextLine()
+		 * int i = 0
+		 * boolean areWeDone = false
+		 */
+		String hint = "Your hint is: 'Vacationland'";
+		String word = "Maine";
+		int i = 0;
+		boolean areWeDone = false;
+		Scanner sc = new Scanner(System.in);
+		/*
+		 * Logic is as follows:
+		 * Welcome user - done
+		 * create mask for word to print to console
+		 * prompt user to guess letter
+		 * if user input reflects a character in the string peel away mask
+		 * until game is over
+		 */
+		for (i = 4; i < word.length(); i++)
+			i = 4; //Creating test condition that will become false when user guesses correctly
 		{
 			System.out.print("*");
-		}
-	String n = wordParts[3];
-		for (int i = 0; i < n.length(); i++);
-		{
-			System.out.print("*");
-		}
-	String e = wordParts[4];
-		for (int i = 0; i <e.length(); i++);
-		{
-			System.out.print("*");
-		}
-		{
-			System.out.println(" ");
-		}
-		{
-			System.out.print("Guess a letter : ");
-		}
 		
-		
-	Scanner sc = new Scanner(System.in);
-		 guess = sc.nextLine(); // find correct variable here
-		{
-			System.out.print("The user has guessed : " + guess);
 		}
-		{
-			System.out.println(" ");
-		}
+		//Here is where 'if' statement goes for when test condition becomes false
+		//mask will hopefully get peeled away
 		
 }
 }
-}
-
+		
+		
+	
+		
+			
+		
+			
 		
 
 
--- CIS-2018-v2/src/HangmanGame/HangmanGameApplication.java
+++ CIS-2018-v2/src/HangmanGame/HangmanGameApplication.java
@@ -1,42 +1,92 @@
 package HangmanGame;
 
 public class HangmanGameApplication {
-	public static void main (String[] args) {
-		/* Current game state represented by looped asterics masking remainder of letters
-		 * Prompt user to guess letter
-		 *Receive answer via user
-		 *If answer is true, apply to current game state
-		 *Repeat 
-		 */
+	/**
+	 * Static Methods
+	 * Methods that call methods
+	 * Example runtime errors
+	 * Flow of control
+	 * 
+	 * @author Michael Bourgeois
+	 *
+	 */
+	public class ProceduralDecomposition 
+	{
+		private static final String MESSAGE_FORMAT = " ************* ";
+		private String guesses = "DOG";
+		private String word = "CAT";
 		
+		/**
+		 * Static Methods -
+		 * No object instance is required to call a static method. 
+		 * Static methods can only reference static class member variables.
+		 * 
+		 */
+		public static void formatMessage(String message)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.append(MESSAGE_FORMAT);
+			sb.append(message);
+			sb.append(MESSAGE_FORMAT);
+			
+			System.out.println(sb.toString());
+		}
+		
+		// This calls another method.
+		public void callAnotherMethod()
+		{
+			if(isCGuessed())
+			{
+				System.out.println("C was guessed :) ");
+			}
+			else
+			{
+				System.out.println("C was not guessed :(");
+			}
+		}
+		
+		/**
+		 * This method is called by 'callAnotherMethod' 
+		 * It encapsulates the logic to determine if the letter C was guessed by the user.
+		 * Encapsulation is a pillar of any OO language.
+		 */
+		     
+		public boolean isCGuessed()
+		{
+			if(guesses.contains("C"))
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}	
+		}
+		
+		// Any error in the code at run time
+		public void demonstrateRuntimeErrors()
+		{
+			String word = "giraffe";
+			for(int i = 0; i < word.length(); i++)
+			{
+				System.out.println(word.charAt(i + 1));
+			}
+		}
+		
+		public static void main(String[] args) 
+		{
+			// We do not need to create an object instance to call static methods.
+			ProceduralDecomposition.formatMessage("Hello World!");
+			
+			// Type is ProceduralDecompostion.
+			// Local var name is decomp.
+			// Assign a new instance of the ProceduralDecompostion object to the decomp variable.
+			ProceduralDecomposition decomp = new ProceduralDecomposition();
+			
+			// Call the callAnotherMethod() on the decomp object.
+			decomp.callAnotherMethod();
+			
+			// Demonstrate a runtime error
+			decomp.demonstrateRuntimeErrors();
+		}
 	}
-}
-String m = wordParts[0];
-for (int i = 0; i < m.length(); i++);
-{
-	System.out.print("*");
-}
-String a = wordParts[1];
-for (int i = 0; i < a.length(); i++);
-{
-	System.out.print("*");
-}
-String I = wordParts[2];
-for (int i = 0; i < I.length(); i++);
-{
-	System.out.print("*");
-}
-String n = wordParts[3];
-for (int i = 0; i < n.length(); i++);
-{
-	System.out.print("*");
-}
-String e = wordParts[4];
-for (int i = 0; i <e.length(); i++);
-{
-	System.out.print("*");
-}
-{
-	System.out.println(" ");
-}
-{
